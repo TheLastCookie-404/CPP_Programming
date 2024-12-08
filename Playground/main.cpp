801783208289865767faf47d8c1be777909829d8
TheLastCookie-404 <se660745@gmail.com>
2024-12-08 21:11:02 +0500

    Hiearachy

---
+++ Playground/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printStr(const char *str) 
+{
+  cout << str << endl;
+}
+
+int main()
+{
+  cout << "lol" << endl;
+  printStr("kek");
+  return 0;
+}
